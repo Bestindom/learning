ae63bd4dbdd087e0f2e8db6264cb6054f022f3b3
Bestindom <bestindom@gmail.com>
2024-01-25 20:10:49 +0100

    update day 25

---
+++ Laravel/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("./Clases/Ciclo.php");
+
+$ciclo = new Ciclo(1, "DAW", "Desarrollo de Aplicaciones Web");
+
+// $ciclo->setId(1);
+// $ciclo->setInitials("DAW");
+// $ciclo->setName("Desarrollo de Aplicaciones Web");
+
+echo $ciclo;
